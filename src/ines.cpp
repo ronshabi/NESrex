54329dfaaad8f67d37353598a3a3d418ef3ca9f6
Ron Shabi <ron@ronsh.net>
2022-11-19 01:48:45 +0200

    Added: validate iNES header

--- src/ines.cpp
+++ src/ines.cpp
@@ -1,7 +1,9 @@
 #include "ines.h"
 
 // FIXME: remove me and all of my instances
+
 #define Log(msg) std::cout << msg << std::endl;
+#define TempException(msg) std::cerr << msg << std::endl
 
 ines::ines(std::string path)
         :m_path{path}
@@ -12,26 +14,49 @@ ines::ines(std::string path)
     if (m_file.is_open()) {
         // determine size of file in bytes
         m_file.seekg(0, std::ios::end);
-        m_size = m_file.tellg();
-
-        // get to the start
-        m_file.seekg(0, std::ios::beg);
-
-
-        // FIXME: unsafe, remove when implementing r/w from file
-        char buffer[100];
-        (m_file.read(buffer, 100));
+        m_size = (int)m_file.tellg();
+        
+        validate();
 
 
-
-
-        // close the file
-        m_file.close();
     }
     else {
         // FIXME: add an exception to file not being able to open
-        // for now...
         Log("Error opening the file, does it exist?");
     }
+}
 
+ines::~ines() {
+    // close the file
+    m_file.close();
+}
+
+// Read (endian neutral)
+std::vector<uint8_t> ines::read(int offset, int size) {
+    // validate offset
+    if (offset <= m_size) {
+        std::vector<uint8_t> ret;
+        char current[1];
+
+        for (int i = 0; i < size; i++) {
+            m_file.seekg(offset+i, std::ios::beg);
+            m_file.read(current, 1);
+            ret.insert(ret.cend(), *current);
+        }
+
+        return ret;
+
+    } else {
+        TempException("Wrong file offset chosen");
+        return std::vector<uint8_t> {0xff};
+    }
+}
+
+// This function validates the iNes File.
+bool ines::validate() {
+    // Validate header
+    std::vector<uint8_t> header = read(0, 4);
+    if (header == std::vector<uint8_t>{0x4e, 0x45, 0x53, 0x1a}) {
+        Log("valid");
+    }
 }
--- src/ines.h
+++ src/ines.h
@@ -3,16 +3,21 @@
 
 #include <fstream>
 #include <iostream>
-
+#include <array>
+#include <vector>
 
 class ines {
 public:
     ines(std::string path);
+    ~ines();
 
 private:
     std::string m_path;
     std::fstream m_file;
-    int m_size;
+    int m_size = -1;
+
+    bool validate();
+    std::vector<uint8_t> read(int offset, int size);
 };
 
 #endif //NESREX_INES_H
