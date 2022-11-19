1c397cded8fd69e9b3aa6be9c3f13b4253966fc1
Ron Shabi <ron@ronsh.net>
2022-11-19 02:07:53 +0200

    ines: m_valid as a class member

--- src/ines.cpp
+++ src/ines.cpp
@@ -1,10 +1,10 @@
 #include "ines.h"
 
 // FIXME: remove me and all of my instances
-
 #define Log(msg) std::cout << msg << std::endl;
+// FIXME: replace me with an exception
 #define TempException(msg) std::cerr << msg << std::endl
-
+#define PrintHex(hex) printf("%02x ", hex)
 ines::ines(std::string path)
         :m_path{path}
 {
@@ -15,13 +15,17 @@ ines::ines(std::string path)
         // determine size of file in bytes
         m_file.seekg(0, std::ios::end);
         m_size = (int)m_file.tellg();
-        
+
         validate();
 
+        if (m_valid) {
+
+        } else {
+            TempException("File isn't valid");
+        }
 
     }
     else {
-        // FIXME: add an exception to file not being able to open
         Log("Error opening the file, does it exist?");
     }
 }
@@ -34,7 +38,7 @@ ines::~ines() {
 // Read (endian neutral)
 std::vector<uint8_t> ines::read(int offset, int size) {
     // validate offset
-    if (offset <= m_size) {
+    if (offset <= m_size && offset >= 0) {
         std::vector<uint8_t> ret;
         char current[1];
 
@@ -45,7 +49,6 @@ std::vector<uint8_t> ines::read(int offset, int size) {
         }
 
         return ret;
-
     } else {
         TempException("Wrong file offset chosen");
         return std::vector<uint8_t> {0xff};
@@ -53,10 +56,12 @@ std::vector<uint8_t> ines::read(int offset, int size) {
 }
 
 // This function validates the iNes File.
-bool ines::validate() {
+void ines::validate() {
     // Validate header
     std::vector<uint8_t> header = read(0, 4);
+
     if (header == std::vector<uint8_t>{0x4e, 0x45, 0x53, 0x1a}) {
         Log("valid");
+        m_valid = true;
     }
 }
--- src/ines.h
+++ src/ines.h
@@ -14,9 +14,10 @@ public:
 private:
     std::string m_path;
     std::fstream m_file;
+    bool m_valid = false;
     int m_size = -1;
 
-    bool validate();
+    void validate();
     std::vector<uint8_t> read(int offset, int size);
 };
 
